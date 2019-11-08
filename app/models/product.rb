5055c327df624b783b497026596eee78c0fa7df2
Danny <dannynairn20@gmail.com>
2019-11-07 20:02:04 -0600

    Installed active storage and migrated database

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :item, :description, :price, :stock, presence: true
+  validates :stock, numericality: {only_integer: true}
+  validates :stock, :percentDiscount, numericality: true
 end
