be08765c9281e56cc4724fb9a7fad4b673694566
Danny <dannynairn20@gmail.com>
2019-11-25 21:49:46 -0600

    Migrated after OrderProduct model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_034233) do
+ActiveRecord::Schema.define(version: 2019_11_26_034827) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -86,6 +86,17 @@ ActiveRecord::Schema.define(version: 2019_11_26_034233) do
     t.index ["province_id"], name: "index_customers_on_province_id"
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
+    t.integer "quantity"
+    t.float "purchasePrice"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "status_id", null: false
@@ -133,6 +144,8 @@ ActiveRecord::Schema.define(version: 2019_11_26_034233) do
   add_foreign_key "category_products", "Categories"
   add_foreign_key "category_products", "Products"
   add_foreign_key "customers", "provinces"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "orders", "customers"
   add_foreign_key "orders", "statuses"
 end
