b00f318ef443d2afa642e7696105e80f84323c31
Danny <dannynairn20@gmail.com>
2019-11-25 21:27:14 -0600

    Added ability to add categories to products with out directly manipulating the active record join table

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,11 +1,14 @@
 ActiveAdmin.register Product do
-    permit_params :item, :description, :price, :stock, :percentDiscount, :image
+    permit_params :item, :description, :price, :stock, :percentDiscount, :image, category_products_attributes: [:id, :Product_id, :Category_id, :_destroy]
 
     form do |f|
-      f.semantic_errors
+      f.semantic_errors *f.object.errors.keys
       f.inputs
       f.inputs do
         f.input :image, as: :file
+        f.has_many :category_products, allow_destroy: true do |n_f|
+          n_f.input :Category
+        end
       end
       f.actions
     end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
 
-  has_many :category_products
+  has_many :category_products, foreign_key: "Product_id"
   has_many :Categories, through: :category_products
+
+  accepts_nested_attributes_for :category_products, allow_destroy: true
 
   validates :item, :description, :price, :stock, presence: true
   validates :stock, numericality: {only_integer: true}
