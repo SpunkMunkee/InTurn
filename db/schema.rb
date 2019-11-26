ad84ac354c4d831ad0290d9c85e395dee193a54a
Danny <dannynairn20@gmail.com>
2019-11-25 21:41:05 -0600

    Migrated after status model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_033622) do
+ActiveRecord::Schema.define(version: 2019_11_26_033848) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -109,6 +109,12 @@ ActiveRecord::Schema.define(version: 2019_11_26_033622) do
     t.float "hst"
     t.float "pst"
     t.float "qst"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
